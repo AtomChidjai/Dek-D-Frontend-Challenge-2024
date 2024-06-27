ef13e71f61033fec45248103f8a0c0cf7df50f34
AtomChidjai <atomchidjai@gmail.com>
2024-06-27 23:49:13 +0700

    test add & remove button

--- front-end-project/src/components/Container.jsx
+++ front-end-project/src/components/Container.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import Card from './Card'
+import { useState } from 'react'
+import { IoAddCircleOutline } from "react-icons/io5";
+import { IoIosRemoveCircleOutline } from "react-icons/io";
 
 const Container = () => {
-  return (
-    <>
-        <div className='w-[1050px] flex flex-wrap'>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-        </div>
-    </>
-  )
+    const [cardCount, setCardCount] = useState(7);
+
+    const addCard = () => {
+        setCardCount(prevCount => prevCount + 1);
+    }
+
+    const removeCard = () => {
+        if (cardCount > 1) {
+            setCardCount(prevCount => prevCount - 1);
+        }
+    }
+
+    return (
+        <>
+            <button className='text-[50px]' onClick={addCard}><IoAddCircleOutline/></button>
+            <button className='text-[50px]' onClick={removeCard}><IoIosRemoveCircleOutline/></button>
+            <div className='w-[1050px] flex flex-wrap'>
+            {[...Array(cardCount)].map((_, index) => (
+                <Card key={index + 1} number={index + 1} />
+            ))}
+            </div>
+        </>
+    )
 }
 
 export default Container
