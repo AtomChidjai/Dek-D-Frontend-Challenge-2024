e97d3bc285d16d72cedc3c421c97d83a5e538444
AtomChidjai <atomchidjai@gmail.com>
2024-06-29 00:13:35 +0700

    add select toggle button

--- front-end-project/src/components/Container.jsx
+++ front-end-project/src/components/Container.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 
 const Container = () => {
     const [cardCount, setCardCount] = useState(7);
+    const [selectToggle, setSelectToggle] = useState(false);
 
     const addCard = () => {
         setCardCount(prevCount => prevCount + 1);
@@ -15,6 +16,10 @@ const Container = () => {
         }
     }
 
+    const selectHandler = () => {
+        setSelectToggle(toggle => !toggle);
+    }
+
     return (
         <>
             <div className='w-full flex flex-col items-center'>
@@ -22,7 +27,14 @@ const Container = () => {
                     <span className='text-[12px] text-[#878787]'>จํานวนทั้งหมด {cardCount} รายการ</span>
                     <div className='flex items-center'>
                         <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px]' onClick={addCard}>เพิ่ม</button>
-                        <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px] ml-3' onClick={removeCard}>เเก้ไข</button>
+
+                        { selectToggle && <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px] ml-3' onClick={selectHandler}>ยกเลิก</button>}
+                        
+                        { selectToggle ? 
+                            <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px] ml-3' onClick={removeCard}>ลบรายการ</button> : 
+                            <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px] ml-3' onClick={selectHandler}>เเก้ไข</button> 
+                        }
+                    
                     </div>
                 </div>
                 <div className='w-[1100px] flex flex-wrap'>
