6ebea5ea67a6f706fa4b50553045c7dbebbd074b
AtomChidjai <atomchidjai@gmail.com>
2024-06-29 15:40:05 +0700

    add Selection Functinolity

--- front-end-project/src/components/Card.jsx
+++ front-end-project/src/components/Card.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { IoMdBookmark } from "react-icons/io";
 import { IoList } from "react-icons/io5";
 
-const Card = ({number}) => {
+const Card = ({ number, isSelected, onSelect }) => {
   return (
     <>
-        <div className='w-[350px] h-auto flex flex-row rounded-lg mx-2 my-3'>
+        <div className={`w-[350px] h-auto flex flex-row rounded-lg mx-2 my-3 ${isSelected ? 'border-2 border-blue-500' : ''}`} onClick={onSelect}>
             <img src="https://placehold.co/100x150"  className='rounded-lg'/>
             <div className='flex flex-col justify-between ml-[5px]'>
                 <div className=''>
--- front-end-project/src/components/Container.jsx
+++ front-end-project/src/components/Container.jsx
@@ -1,50 +1,58 @@
-import React from 'react'
-import Card from './Card'
-import { useState } from 'react'
+import React, { useState } from 'react';
+import Card from './Card';
 
 const Container = () => {
     const [cardCount, setCardCount] = useState(7);
     const [selectToggle, setSelectToggle] = useState(false);
+    const [selectedCards, setSelectedCards] = useState([]);
 
     const addCard = () => {
         setCardCount(prevCount => prevCount + 1);
-    }
+    };
 
-    const removeCard = () => {
-        if (cardCount > 1) {
-            setCardCount(prevCount => prevCount - 1);
+    const removeSelectedCards = () => {
+        const remainingCards = [...Array(cardCount)].map((_, i) => i).filter(i => !selectedCards.includes(i));
+        setCardCount(remainingCards.length);
+        setSelectedCards([]);
+    };
+
+    const toggleSelectCard = (index) => {
+        if (!selectToggle) {
+            return;
         }
-    }
+        setSelectedCards(prev => {
+            if (prev.includes(index)) {
+                return prev.filter(cardIndex => cardIndex !== index);
+            } else {
+                return [...prev, index];
+            }
+        });
+    };
 
-    const selectHandler = () => {
+    const toggleSelectionMode = () => {
         setSelectToggle(toggle => !toggle);
-    }
+        setSelectedCards([]);
+    };
 
     return (
-        <>
-            <div className='w-full flex flex-col items-center'>
-                <div className='w-[1100px] flex px-2 justify-between items-center'>
-                    <span className='text-[12px] text-[#878787]'>จํานวนทั้งหมด {cardCount} รายการ</span>
-                    <div className='flex items-center'>
-                        <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px]' onClick={addCard}>เพิ่ม</button>
-
-                        { selectToggle && <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px] ml-3' onClick={selectHandler}>ยกเลิก</button>}
-                        
-                        { selectToggle ? 
-                            <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px] ml-3' onClick={removeCard}>ลบรายการ</button> : 
-                            <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px] ml-3' onClick={selectHandler}>เเก้ไข</button> 
-                        }
-                    
-                    </div>
-                </div>
-                <div className='w-[1100px] flex flex-wrap'>
-                {[...Array(cardCount)].map((_, index) => (
-                    <Card key={index + 1} number={index + 1} />
-                ))}
+        <div className='w-full flex flex-col items-center'>
+            <div className='w-[1100px] flex px-2 justify-between items-center'>
+                <span className='text-[12px] text-[#878787]'>จํานวนทั้งหมด {cardCount} รายการ</span>
+                <div className='flex items-center'>
+                    <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px]' onClick={addCard}>เพิ่ม</button>
+                    {selectToggle && <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px] ml-3' onClick={toggleSelectionMode}>ยกเลิก</button>}
+                    {selectToggle ? 
+                        <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px] ml-3' onClick={removeSelectedCards}>ลบรายการ</button> : 
+                        <button className='text-[12px] border border-[#E5E5E5] px-4 py-2 rounded-[20px] ml-3' onClick={toggleSelectionMode}>เเก้ไข</button>}
                 </div>
             </div>
-        </>
-    )
-}
+            <div className='w-[1100px] flex flex-wrap'>
+                {[...Array(cardCount)].map((_, index) => (
+                    <Card key={index + 1} number={index + 1} isSelected={selectedCards.includes(index)} onSelect={() => toggleSelectCard(index)} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
-export default Container
+export default Container;
